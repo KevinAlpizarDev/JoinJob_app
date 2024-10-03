9a60d83ab7f3109327ccbb54a4ae812ba4d74803
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-10-03 15:13:42 -0600

    adding service file

---
+++ reactApp/src/services/service.js
@@ -0,0 +1,29 @@
+// services/service.js
+import axios from "axios";
+
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+  baseURL: "http://127.0.0.1:8000", // Ensure this is the correct backend URL
+});
+
+export const getCurrentUser = () => {
+  return client.get("/api/user");
+};
+
+export const registerUser = (email, username, password) => {
+  return client.post("/api/register", { email, username, password });
+};
+
+export const loginUser = (email, password) => {
+  return client.post("/api/login", { email, password });
+};
+
+export const logoutUser = () => {
+  return client.post("/api/logout");
+};
+
+
+
