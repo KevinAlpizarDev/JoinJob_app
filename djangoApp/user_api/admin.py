aa75d14b1187d92e39aaa7d9555b40d2dc8e547a
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-10-04 11:46:00 -0600

    Models registred in admin.py

--- djangoApp/user_api/admin.py
+++ djangoApp/user_api/admin.py
@@ -1,3 +1,10 @@
+# from django.contrib import admin
 from django.contrib import admin
 
-# Register your models here.
+from .models import AppUser, Cursos, Inscripcion, Institucion, Sede
+
+admin.site.register(AppUser)
+admin.site.register(Institucion)
+admin.site.register(Sede)
+admin.site.register(Cursos)
+admin.site.register(Inscripcion)
