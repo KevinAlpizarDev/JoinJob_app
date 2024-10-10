3644820952be5ccdefa9b6c35b675f44875f974d
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-10-10 12:59:53 -0600

    Landing styles

--- reactApp/src/components/LandingPage.jsx
+++ reactApp/src/components/LandingPage.jsx
@@ -7,26 +7,24 @@ export default function LandingPage() {
   return (
     <>
       <NavBar />
-      <Position/>
+      <Position />
       <div className="flex flex-col min-h-screen">
         {/* <Position/> */}
         <main className="flex-1">
-          <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-            <div className="container px-4 md:px-6">
-              <div className="flex flex-col items-center space-y-4 text-center">
-                <div className="space-y-1">
-                  <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                    Refreshingly simple project management
+          <section className="w-full  py-12 md:py-24 lg:py-32 xl:py-48 bg-slate-100 dark:bg-slate-600">
+            <div className="container px-4 md:px-6 ">
+              <div className="flex flex-col items-center space-y-4 text-center ">
+                <div className=" space-y-1">
+                  <h1 className="max-w-5xl text-slate-800 text-2xl sm:text-6xl tracking-tight font-black dark:text-white">
+                    Oportunidades de Capacitación y Proyectos para el Desarrollo
                   </h1>
-                  <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                  <p className="mx-auto font-poppins pt-6 max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                     The project management platform that helps small teams move
                     faster and make more progress than they ever thought
                     possible.
                   </p>
                 </div>
                 <div className="space-x-4">
-             
-
                   <Link to="/signin">
                     <button
                       type="button"
@@ -55,71 +53,76 @@ export default function LandingPage() {
             </div>
           </section>
 
-    
+          <section className="w-full py-12 md:py-24 lg:py-32">
+            <div className="container px-4 md:px-6">
+              <h2 className="text-3xl text-slate-800 font-black tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">
+                Oportunidades de Capacitación y Proyectos para el Desarrollo
+              </h2>
+              <div className="grid gap-6 lg:grid-cols-2 lg:gap-12">
+                <div className="flex items-start space-x-4">
+                  <div className="mt-1 h-6 w-6 flex-none bg-green-500 rounded-full"></div>
+                  <div>
+                    <h3 className="mt-1 text-xl leading-8 tracking-tight font-semibold text-slate-900 dark:text-white">Capacitación para PYMEs</h3>
+                    <p className="text-gray-500 dark:text-gray-400 font-poppins">
+                      Programas de formación para fortalecer pequeñas y medianas
+                      empresas, cubriendo temas como ventas, marketing y gestión
+                      empresarial.
+                    </p>
+                  </div>
+                </div>
 
-<section className="w-full py-12 md:py-24 lg:py-32">
-  <div className="container px-4 md:px-6">
-    <h2 className="text-3xl text-slate-800 font-black tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">
-      Oportunidades de Capacitación y Proyectos para el Desarrollo
-    </h2>
-    <div className="grid gap-6 lg:grid-cols-2 lg:gap-12">
-      
-      <div className="flex items-start space-x-4">
-        <div className="mt-1 h-6 w-6 flex-none bg-green-500 rounded-full"></div>
-        <div>
-          <h3 className="font-bold">Capacitación para PYMEs</h3>
-          <p className="text-gray-500 dark:text-gray-400">
-            Programas de formación para fortalecer pequeñas y medianas empresas, cubriendo temas como ventas, marketing y gestión empresarial.
-          </p>
-        </div>
-      </div>
-      
-      <div className="flex items-start space-x-4">
-        <div className="mt-1 h-6 w-6 flex-none bg-blue-500 rounded-full"></div>
-        <div>
-          <h3 className="font-bold">Puente al Desarrollo</h3>
-          <p className="text-gray-500 dark:text-gray-400">
-            Proyecto que busca sacar a las familias de la pobreza extrema mediante el acceso a capacitación y oportunidades laborales.
-          </p>
-        </div>
-      </div>
-      
-      <div className="flex items-start space-x-4">
-        <div className="mt-1 h-6 w-6 flex-none bg-purple-500 rounded-full"></div>
-        <div>
-          <h3 className="font-bold">Empleabilidad de Jóvenes con Discapacidad</h3>
-          <p className="text-gray-500 dark:text-gray-400">
-            Proyectos orientados a la inclusión laboral de jóvenes con discapacidad mediante formación y oportunidades de empleo.
-          </p>
-        </div>
-      </div>
-      
-      <div className="flex items-start space-x-4">
-        <div className="mt-1 h-6 w-6 flex-none bg-yellow-500 rounded-full"></div>
-        <div>
-          <h3 className="font-bold">Educación Dual</h3>
-          <p className="text-gray-500 dark:text-gray-400">
-            Proyecto que combina educación y trabajo en empresas para que los estudiantes adquieran experiencia laboral mientras estudian.
-          </p>
-        </div>
-      </div>
-      
-      <div className="flex items-start space-x-4">
-        <div className="mt-1 h-6 w-6 flex-none bg-red-500 rounded-full"></div>
-        <div>
-          <h3 className="font-bold">Proyectos de Emprendimiento</h3>
-          <p className="text-gray-500 dark:text-gray-400">
-            Asesoría y capital semilla para ayudar a personas en situación vulnerable a iniciar y desarrollar sus propios negocios.
-          </p>
-        </div>
-      </div>
+                <div className="flex items-start space-x-4">
+                  <div className="mt-1 h-6 w-6 flex-none bg-blue-500 rounded-full"></div>
+                  <div>
+                    <h3 className="font-bold">Puente al Desarrollo</h3>
+                    <p className="text-gray-500 dark:text-gray-400 font-poppins">
+                      Proyecto que busca sacar a las familias de la pobreza
+                      extrema mediante el acceso a capacitación y oportunidades
+                      laborales.
+                    </p>
+                  </div>
+                </div>
 
-    </div>
-  </div>
-</section>
+                <div className="flex items-start space-x-4">
+                  <div className="mt-1 h-6 w-6 flex-none bg-purple-500 rounded-full"></div>
+                  <div>
+                    <h3 className="font-bold">
+                      Empleabilidad de Jóvenes con Discapacidad
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400 font-poppins">
+                      Proyectos orientados a la inclusión laboral de jóvenes con
+                      discapacidad mediante formación y oportunidades de empleo.
+                    </p>
+                  </div>
+                </div>
 
+                <div className="flex items-start space-x-4">
+                  <div className="mt-1 h-6 w-6 flex-none bg-yellow-500 rounded-full"></div>
+                  <div>
+                    <h3 className="font-bold">Educación Dual</h3>
+                    <p className="text-gray-500 dark:text-gray-400 font-poppins">
+                      Proyecto que combina educación y trabajo en empresas para
+                      que los estudiantes adquieran experiencia laboral mientras
+                      estudian.
+                    </p>
+                  </div>
+                </div>
 
-    
+                <div className="flex items-start space-x-4">
+                  <div className="mt-1 h-6 w-6 flex-none bg-red-500 rounded-full"></div>
+                  <div>
+                    <h3 className="font-bold">Proyectos de Emprendimiento</h3>
+                    <p className="text-gray-500 dark:text-gray-400 font-poppins">
+                      Asesoría y capital semilla para ayudar a personas en
+                      situación vulnerable a iniciar y desarrollar sus propios
+                      negocios.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
           <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
             <div className="container px-4 md:px-6">
               <h2 className="text-3xl font-black text-slate-800 tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">
@@ -215,7 +218,3 @@ export default function LandingPage() {
     </>
   );
 }
-
-
-
-
