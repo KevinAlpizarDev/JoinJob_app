b4c92c09b3fd0e11952df5113d4fa2c2f5b393e6
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-10-10 11:22:10 -0600

    util in logout to export

--- reactApp/src/pages/AdminPage.jsx
+++ reactApp/src/pages/AdminPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+// AdminPage.jsx
+import React from "react";
+import { useLogout } from "../utils/authUtils"; // Ajusta la ruta según sea necesario
 
 const AdminPage = () => {
+  const submitLogout = useLogout(); // Usa la función de logout
+
   return (
     <div>
       <h1>Si Soy Admin! :)</h1>
+      <form onSubmit={submitLogout}>
+        <button type="submit">Log out</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
---
+++ reactApp/src/utils/authUtils.js
@@ -0,0 +1,16 @@
+// authUtils.js
+import { useAuth } from "../components/AuthProvider"; // Ajusta la ruta según sea necesario
+import { useNavigate } from "react-router-dom";
+
+export const useLogout = () => {
+  const { setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const submitLogout = (e) => {
+    e.preventDefault();
+    setUser(null); // Limpia el estado del usuario
+    navigate("/signin"); // Redirige a la página de inicio de sesión
+  };
+
+  return submitLogout;
+};
