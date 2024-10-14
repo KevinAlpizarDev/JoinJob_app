beae1f5e825b7d930ba7486a53d5de4d05e25fdd
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-10-14 09:05:36 -0600

    Key

--- reactApp/src/components/CourseList.jsx
+++ reactApp/src/components/CourseList.jsx
@@ -1,16 +1,46 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+// import React, { useState } from "react";
+// import { useEffect } from "react";
+// import { getAllCourses } from "../services/service";
+
+// const CourseList = () => {
+//   const [courses, setCourses] = useState([]);
+
+//   useEffect(() => {
+
+//     async function loadCourses() {
+//       const res = await getAllCourses();
+//       //   console.log(res);
+//       setCourses(res.data);
+//     }
+//     loadCourses();
+//   }, []);
+
+//   return (
+//     <div >
+//       <h1>Course CourseList</h1>
+//       {courses.map((course) => (
+//         <div>
+//           <h1>{course.nombre}</h1>
+//           <h1>{course.codigo}</h1>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default CourseList;
+
+//Key
+
+import React, { useState, useEffect } from "react";
 import { getAllCourses } from "../services/service";
 
 const CourseList = () => {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
- 
-
     async function loadCourses() {
       const res = await getAllCourses();
-      //   console.log(res);
       setCourses(res.data);
     }
     loadCourses();
@@ -18,9 +48,11 @@ const CourseList = () => {
 
   return (
     <div>
-      <h1>Course CourseList</h1>
+      <h1>Course List</h1>
       {courses.map((course) => (
-        <div>
+        <div key={course.id}>
+          {" "}
+          {/* Assuming each course has a unique id */}
           <h1>{course.nombre}</h1>
           <h1>{course.codigo}</h1>
         </div>
