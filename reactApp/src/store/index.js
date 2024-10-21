b4b9a6ab70c508ad2382ac0762da8448fe863d3a
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-10-21 12:52:09 -0600

    Dejar de rastrear la carpeta src

--- reactApp/src/store/index.js
+++
@@ -1,9 +0,0 @@
-// src/store/index.js
-import { configureStore } from "@reduxjs/toolkit";
-import themeReducer from "./themeSlice";
-
-export const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
-});
